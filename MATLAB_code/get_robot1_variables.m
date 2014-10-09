528fb4d0c27f10f2dc13e6a96ed5f2d8cc764610
Eza Koch <koch.eza@gmail.com>
2014-10-08 21:55:41 -0400

    added test file

--- MATLAB_code/get_robot1_variables.m
+++ MATLAB_code/get_robot1_variables.m
@@ -44,10 +44,18 @@ userdata.team.robot1.diff_theta(i) = str2double(data);
 set(userdata.h.robot1.var8_value, 'String',userdata.team.robot1.diff_theta(i))
 
 
-% mean_camera_y = mean(userdata.team.robot1.camera_x)
-% mean_camera_x = mean(userdata.team.robot1.camera_y)
-% temp_x = userdata.team.robot1.x(i)
-% temp_y = userdata.team.robot1.y(i)
+mean_camera_y = mean(userdata.team.robot1.camera_x)
+mean_camera_x = mean(userdata.team.robot1.camera_y)
+temp_x = userdata.team.robot1.x(i)
+temp_y = userdata.team.robot1.y(i)
+% mean_camera_y =
+% 
+%   151.6000
+% 
+% 
+% mean_camera_x =
+% 
+%   -20.3000
 
 
 end
